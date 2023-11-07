25170da458bfd06f8c01e2b18a70b9fd00faf2bf
alibeyram <ali.beyram.m@gmail.com>
2023-11-07 14:19:51 -0800

    add the correct imports

--- iOS/src/toga_iOS/libs/dispatch.py
+++ iOS/src/toga_iOS/libs/dispatch.py
@@ -3,21 +3,26 @@
 ##########################################################################
 from ctypes import (
     Structure,
+    byref,
     c_char_p,
     c_int64,
-    c_void_p,
     c_uint64,
     c_ulong,
+    c_void_p,
+    cast,
     cdll,
     util,
 )
 
+from rubicon.objc import ObjCInstance
 from rubicon.objc.runtime import objc_id
+
 ######################################################################
 libdispatch = cdll.LoadLibrary(util.find_library("libdispatch"))
 ######################################################################
 
-#libdispatch = load_library("System")
+# libdispatch = load_library("System")
+
 
 class struct_dispatch_queue_s(Structure):
     pass  # No _fields_, because this is an opaque structure.
@@ -35,7 +40,7 @@ libdispatch.dispatch_queue_create.argtypes = [c_char_p, objc_id]
 
 libdispatch.dispatch_after.restype = c_void_p
 libdispatch.dispatch_after.argtypes = [c_uint64, objc_id, objc_id]
-#(dispatch_time_t when, dispatch_queue_t queue, dispatch_block_t block);
+# (dispatch_time_t when, dispatch_queue_t queue, dispatch_block_t block);
 
 libdispatch.dispatch_block_create.restype = objc_id
 libdispatch.dispatch_block_create.argtypes = [c_ulong, objc_id]
@@ -45,7 +50,7 @@ libdispatch.dispatch_block_cancel.argtypes = [objc_id]
 
 libdispatch.dispatch_time.restype = c_uint64
 libdispatch.dispatch_time.argtypes = [c_uint64, c_int64]
-#(dispatch_block_flags_t flags, dispatch_block_t block);
+# (dispatch_block_flags_t flags, dispatch_block_t block);
 
 DISPATCH_TIME_NOW = 0
 NSEC_PER_SEC = 1000000000
